911845b7c967f12fb36bcddfc17d7ce84829f6f7
David Schlenker <david.schlenker.webdev@gmail.com>
2023-10-21 19:53:35 -0600

    Scaling and Positioning
    Also Included Download Button on Ionic Tab.

---
+++ Vanilla/index.js
@@ -0,0 +1,25 @@
+function loadInit() {
+    console.log(`index.js loaded!`);
+    $('.nav-link').click(function (e) {  
+        //get selected href
+        var href = $(this).attr('href');    
+        // console.log(`${href} nav clicked!`);
+        
+        //Fade
+        $(`.buttonBox`).removeClass(`d-flex`);
+        $(`.buttonBox`).removeClass(`show`);
+        $(`.buttonBox`).removeClass(`active`);
+
+
+        //Add Classes to element selected by href id
+        $(`${href}-Btns`).addClass(`d-flex`);
+        $(`${href}-Btns`).addClass(`show`);
+        $(`${href}-Btns`).addClass(`active`);
+
+
+        // $(`${href}-Btns a`).click((e) => {
+        //     console.log(this)
+        // })
+    });
+};
+
